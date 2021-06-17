c181e662685855664824546a1e684807bfafd757
ZardoxGit <sounouk@hotmail.fr>
2021-06-17 11:59:51 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='app'>
+    <main>
+      <div class='search-box'>
+        <input type='text' class='search-bar' placeholder='Ville...' v-model="cities" @keypress="fetchWeather"/>
+      </div>
+      <div class='weather-wrap' >
+        <div class='location-box'>
+          <div class='location'>{{ weather.name }}</div>
+          <div class='date'>{{ date() }}</div>
+        </div>
+      
+        <div class='weather-box'>
+          <div class='temp'>{{ weather.main.temp }}°C</div>
+          <div class='weather'>Soleil</div>
+        </div>
+      </div>
+    </main>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      api_key: '43a97c01ee587826f1cf74c5564bfb3a',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      cities: '',
+      weather: {}
+    }
+  },
+  methods: {
+    fetchWeather (e) {
+      if (e.key == 'Enter') {
+        fetch(`${this.url_base}weather?q=${this.cities}&units=metric&appid=${this.api_key}`)
+        .then(res => {
+          return res.json();
+        })
+        .then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
+    },
+  
+      date() {
+        var today = new Date();
+        var date = `${today.getDate()}/${today.getMonth()+1}/${today.getFullYear()}`;
+        return date;
+      }
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  
+ 
 </style>
